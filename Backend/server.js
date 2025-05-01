d2d561ca38c77f790c6b47ef908fbc317accc464
Vanjula <vanjulasankar@gmail.com>
2025-05-01 19:31:13 +0530

    group controller,service,router

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
+const groupRouter = require('./routers/grouprouters.js');
 
 dotenv.config();
 const app=express();
 const PORT =process.env.PORT;
 
 connectDB();
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello first page");
+})
+
+app.use('/groups',groupRouter);
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port "+PORT);
