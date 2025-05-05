5c7abf8a2777d3d5090828b5226ec4a92eb01f48
Vanjula <vanjulasankar@gmail.com>
2025-05-05 14:29:15 +0530

    frontend header

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,8 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from "../components/common/Header.jsx";
+
+export default function AppRoutes() {
+    return (
+        <Header/>
+    );
+}
